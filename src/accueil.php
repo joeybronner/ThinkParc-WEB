71f9714d73466bce2bf3a9cb5937eafaf6bbfe70
Joey Bronner <joeybronner@gmail.com>
2015-04-12 19:47:16 +0200

    Footer fixed bottom

--- src/accueil.php
+++ src/accueil.php
@@ -526,14 +526,11 @@
                             </div>
 							</section>
             </div><!-- /.row --> 
-
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer fixed">
-                    <p class="footer-text">Copyright &copy; 2015 FCT Partners</p>
-                </div> 
-            </div>
-
 		</div>
-      
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer fixed">
+                <p class="footer-text">Copyright &copy; 2015 FCT Partners</p>
+            </div> 
+		</div>      
     </body> 
 </html>
