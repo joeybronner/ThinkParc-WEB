8725ae1b676694a9f7981c0ac8b92d844f3d772d
Joey Bronner <joeybronner@gmail.com>
2015-06-29 18:58:43 +0200

    Reporting for parts (by site, transfert & maintenance)

--- src/reporting/partsconsumption.php
+++ src/reporting/partsconsumption.php
@@ -48,21 +48,37 @@
 	<script>
 		// Load charts
 		var data_charts1;
+		var data_charts3;
+		var data_charts6;
 		google.load("visualization", "1", {packages:["corechart"]});
-		google.setOnLoadCallback(drawChart);
+		google.setOnLoadCallback(drawChart6);
 		$(function onLoad(){
 			document.getElementById("title_chart1").style.display = "none";
+			document.getElementById("title_chart3").style.display = "none";
+			document.getElementById("title_chart4").style.display = "none";
+			document.getElementById("chart4").style.display = "none";
+			document.getElementById("title_chart5").style.display = "none";
+			document.getElementById("chart5").style.display = "none";
+			document.getElementById("title_chart6").style.display = "none";
+			getAllReferences();
 			$('#date_start').datepicker();
 			$('#date_end').datepicker();
 		});
 		function refreshReport() {
 			var sum_parts = 0;
 			document.getElementById("title_chart1").style.display = "block";
+			document.getElementById("title_chart3").style.display = "block";
+			document.getElementById("title_chart4").style.display = "block";
+			document.getElementById("chart4").style.display = "block";
+			document.getElementById("title_chart5").style.display = "block";
+			document.getElementById("chart5").style.display = "block";
+			document.getElementById("title_chart6").style.display = "block";
 			var reference = document.getElementById("reference").value;
 			var date_start = document.getElementById("date_start").value.split("/").reverse().join("-");
 			var date_end = document.getElementById("date_end").value.split("/").reverse().join("-");
 			var filter = document.getElementById("filter").value;
 			getCompany(function(company){
+				// Chart for part consumption (maintenance)
 				$.ajax({
 					method: 	"GET",
 					url:		"http://think-parc.com/webservice/v1/companies/" + company + "/reporting/" +
@@ -91,13 +107,133 @@
 									}
 									drawChart();
 									document.getElementById("sum_parts").innerHTML = sum_parts;
-									document.getElementById("text_usedparts").innerHTML = "parts used between " + 
+									document.getElementById("text_usedparts").innerHTML = "parts used for maintenance between " + 
 																								reformatDate(date_start) + 
 																								" and " + 
 																								reformatDate(date_end);
 								}
 				});
+				// Chart for part consumption (transfert)
+				$.ajax({
+					method: 	"GET",
+					url:		"http://think-parc.com/webservice/v1/companies/" + company + "/reporting/" +
+																	"reference/transfert/" + reference +
+																	"/start/" + date_start +
+																	"/end/" + date_end +
+																	"/filter/" + filter,  
+					success:	function(data) {
+									var response = JSON.parse(data);
+									data_charts3 = new Array(response.length+1);
+									data_charts3[0] = new Array( "Part", "Transfert", { role: "style" } );
+									for (var i = 1; i<=response.length; i++) {
+										sum_parts = sum_parts + parseInt(response[i-1].somme);
+										var col_name;
+										if (filter == "DAY") {
+											col_name = reformatDate(response[i-1].f);
+										} else if (filter == "MONTH") {
+											col_name = response[i-1].f + "/" + response[i-1].y;
+										} else if (filter == "YEAR") {
+											col_name = response[i-1].y;
+										}
+										
+										data_charts3[i] = new Array( 	col_name, 
+																		parseInt(response[i-1].somme), 
+																		"#BADC88" );
+									}
+									drawChart3();
+								}
+				});
+				// Stock quantity & Market value
+				$.ajax({
+					method: 	"GET",
+					url:		"http://think-parc.com/webservice/v1/companies/" + company + "/reporting/reference/" + reference + "/stockvalue",  
+					success:	function(data) {
+									var response = JSON.parse(data);
+									document.getElementById("chart4").innerHTML = response[0].stockquantity;
+									document.getElementById("chart5").innerHTML = response[0].marketvalue + " " + response[0].symbol;
+								}
+				});
+				// Draw chart by sites
+				$.ajax({
+					method: 	"GET",
+					url:		"http://think-parc.com/webservice/v1/companies/" + company + "/reporting/reference/" + reference + "/localisation",  
+					success:	function(data) {
+									var response = JSON.parse(data);
+									data_charts6 = new Array(response.length+1);
+									data_charts6[0] = new Array( "Site", "Quantity" );
+									for (var i = 1; i<=response.length; i++) {
+										data_charts6[i] = new Array( 	response[i-1].name, 
+																		parseInt(response[i-1].partssum)
+																	);
+									}
+									drawChart6();
+								}
+				});
 			});
+		}
+		function drawChart6() {
+			var data;
+			if (typeof(data_charts6) != "undefined") {
+			  data = google.visualization.arrayToDataTable(data_charts6);
+			} else {
+			  data = google.visualization.arrayToDataTable([
+					['Site', 'Quantity'],
+					['SiteA', 0],
+					['SiteB', 0]
+				  ]);
+			}
+
+			  var options = {
+				backgroundColor: {
+				  fill: 'transparent'
+				},
+				pieHole: 0.4,
+				titleTextStyle: {
+				  color: '#FFF'
+				},
+				legendTextStyle: {
+				  color: '#FFF'
+				},
+				/*colors: ['#009933', "#990000"]*/
+			  };
+
+			  var chart = new google.visualization.PieChart(document.getElementById('partslocalisation_values'));
+			  chart.draw(data, options);
+			
+		}
+		function drawChart3() {
+			if (typeof(data_charts3) != "undefined") {
+				var data = google.visualization.arrayToDataTable(data_charts3);
+				var optionsData = {
+					backgroundColor: { fill:'transparent' },
+					titleTextStyle: { color: '#FFF' },
+					legendTextStyle: { color: '#FFF' },
+					vAxis:	{ textStyle: {color: '#FFF' }},
+					hAxis: 	{ textStyle: { color: '#FFF' } },
+					legend: { position: "none" },
+				};
+				var view = new google.visualization.DataView(data);
+				view.setColumns([ 0, 1, { 	calc: "stringify",
+											sourceColumn: 1,
+											type: "string",
+											role: "annotation" }, 2]);
+				var optionsView = {
+					backgroundColor: { fill:'transparent' },
+					titleTextStyle: { color: '#FFF' },
+					legendTextStyle: { color: '#FFF' },
+					bar: {groupWidth: "95%"},
+					vAxis:	{ textStyle: {color: '#FFF' }},
+					hAxis: 	{ textStyle: { color: '#FFF' } },
+					legend: { position: "none" },
+					animation:{
+						duration: 1000,
+						easing: 'in'
+					}
+				};
+				var chart = new google.visualization.ColumnChart(document.getElementById("consumptiontransfert_values"));
+				chart.draw(data, optionsData);	// 1st draw with data (animate)
+				chart.draw(view, optionsView);	// 2nd draw with data & view
+			}
 		}
 		function drawChart() {
 			if (typeof(data_charts1) != "undefined") {
@@ -144,6 +280,22 @@
 							}
 			});
 		};
+		function getAllReferences() {
+			getCompany(function(company) {
+			  $.ajax({
+				  method: "GET",
+				  url: "http://think-parc.com/webservice/v1/companies/" + company + "/reporting/parts/all",
+				  success: function(data) {
+					var response = JSON.parse(data);
+					var content = '';
+					for (var i = 0; i < response.length; i++) {
+					  content = content + '<option value="' + response[i].reference + '">' + response[i].reference + '</option>';
+					}
+					document.getElementById("reference").innerHTML = content;
+				  }
+				});
+			});
+		}
 		function reformatDate(dateStr) {
 			dArr = dateStr.split("-");
 			return dArr[2]+ "/" +dArr[1]+ "/" +dArr[0];
@@ -189,10 +341,9 @@
 										</tr>
 										<tr>
 											<td>
-												<input type="text" class="form-control" id="reference" placeholder="Here, type the part reference" />
-												<!--<select id="listvehicles" name="listvehicles" class="form-control">-->
-													<!-- Retrieve all vehicles with an AJAX [GET] query -->
-												<!--</select>-->
+												<select id="reference" name="reference" required="required" class="form-control">
+													<!-- Retrieve kinds with an AJAX [GET] query -->
+												</select>
 											</td>
 											<td>
 												<input type="text" class="form-control" data-date-format="dd/mm/yyyy" id="date_start" name="date_start" placeholder="JJ/MM/AAAA" required>
@@ -216,7 +367,7 @@
 										<tr id="title_chart1">
 											<td style="padding-top:20px;" colspan="4">
 												<center>
-													<h2>Part using by month</h2>
+													<h2>Parts consumed for maintenance</h2>
 												</center>
 											</td>
 										</tr>
@@ -231,6 +382,46 @@
 											</td>
 											<td id="text_usedparts" style="font-size:2vw" colspan="3">
 												 
+											</td>
+										</tr>
+										<tr id="title_chart3">
+											<td style="padding-top:20px;" colspan="4">
+												<center>
+													<h2>Parts transfered between different sites</h2>
+												</center>
+											</td>
+										</tr>
+										<tr id="chart3">
+											<td id="consumptiontransfert_values" style="max-height:500px;" colspan="4">
+												
+											</td>
+										</tr>
+										<tr id="title_chart4">
+											<td align="center" colspan="4">
+												<h2>Quantity in stock</h2>
+											</td>
+										</tr>
+										<tr>
+											<td id="chart4" align="center" colspan="4" style="font-size:4vw">
+												-
+											</td>
+										</tr>
+										<tr id="title_chart5">
+											<td align="center" colspan="4">
+												<h2>Market value</h2>
+											</td>
+										</tr>
+										<tr>
+											<td id="chart5" align="center" colspan="4" style="font-size:4vw">
+												-
+											</td>
+										</tr>
+										<tr id="title_chart6">
+											<td align="center" colspan="3">
+												<h2>Parts localisation</h2>
+											</td align="center">
+											<td id="partslocalisation_values" style="max-height:500px; margin: 0 auto;" colspan="4">
+													
 											</td>
 										</tr>
 									</tbody>
--- webservice/v1/companies/reporting/Reporting.php
+++ webservice/v1/companies/reporting/Reporting.php
@@ -6,19 +6,66 @@ class Reporting {
      *
      * @url GET /companies/$id_company/reporting/reference/$reference/start/$date_start/end/$date_end/filter/$filter
      */
-    public function getPartPicking($id_company = null, $reference = null, $date_start = null, $date_end = null, $filter = null) {
+    public function getPartUsedForMaintenance($id_company = null, $reference = null, $date_start = null, $date_end = null, $filter = null) {
 		try {
 			global $con;
 			if ($filter == "DAY") {
 				$filter = "";
 			}
-			$sql = 	"SELECT ".$filter."(date_usepart) AS f, YEAR(date_usepart) AS y, SUM(quantity) AS somme ".
-					"FROM partsmaintenance pm, parts p, stock s ".
-					"WHERE pm.id_stock = s.id_stock ".
-					"AND s.id_part = p.id_part ".
-					"AND date_usepart BETWEEN '".$date_start."' AND '".$date_end."' ". 
-					"AND p.reference = '".$reference."' ". 
-					"GROUP BY ".$filter."(date_usepart);";
+			$sql = 	"SELECT concat(".$filter."(dl.date), YEAR(dl.date)) AS entiere, ".$filter."(dl.date) AS f, YEAR(dl.date) AS y, IFNULL(SUM(quantity), 0) AS somme ".
+					"FROM (datelist dl) ".
+					"LEFT JOIN parts p ON p.reference = '".$reference."' ".
+					"LEFT JOIN stock s ON s.id_part = p.id_part ".
+					"LEFT JOIN partsmaintenance pm ON pm.date_usepart = dl.date AND s.id_stock = pm.id_stock ".
+					"WHERE dl.date BETWEEN '".$date_start."' AND '".$date_end."' ".
+					"GROUP BY entiere ".
+					"ORDER BY dl.date ASC;";
+			$stmt = $con->query($sql);
+			return $stmt->fetchAll(PDO::FETCH_OBJ);
+		} catch(PDOException $e) {
+			return array("error" => $e->getMessage());
+		}
+    }
+	
+    /**
+     * Description.
+     *
+     * @url GET /companies/$id_company/reporting/reference/transfert/$reference/start/$date_start/end/$date_end/filter/$filter
+     */
+    public function getPartUsedForTransfert($id_company = null, $reference = null, $date_start = null, $date_end = null, $filter = null) {
+		try {
+			global $con;
+			if ($filter == "DAY") {
+				$filter = "";
+			}
+			$sql = 	"SELECT concat(".$filter."(dl.date), YEAR(dl.date)) AS entiere, ".$filter."(dl.date) AS f, YEAR(dl.date) AS y, IFNULL(SUM(quantity), 0) AS somme ".
+					"FROM (datelist dl) ".
+					"LEFT JOIN parts p ON p.reference = '".$reference."' ".
+					"LEFT JOIN transfert t ON t.transferdate = dl.date AND t.validation = 1 AND p.id_part = t.id_part ".
+					"WHERE dl.date BETWEEN '".$date_start."' AND '".$date_end."' ".
+					"GROUP BY entiere ".
+					"ORDER BY dl.date ASC;";
+			$stmt = $con->query($sql);
+			return $stmt->fetchAll(PDO::FETCH_OBJ);
+		} catch(PDOException $e) {
+			return array("error" => $e->getMessage());
+		}
+    }
+	
+    /**
+     * Description.
+     *
+     * @url GET /companies/$id_company/reporting/reference/$reference/stockvalue
+     */
+    public function getStockValueForReference($id_company = null, $reference = null) {
+		try {
+			global $con;
+			$sql = 	"SELECT IFNULL(SUM(quanty), 0) AS stockquantity, IFNULL((SUM(quanty)*buyingprice), 0) AS marketvalue, IFNULL(symbol, '-') AS symbol ".
+					"FROM parts p, stock s, currencies c ".
+					"WHERE p.reference = '".$reference."' ".
+					"AND p.id_part = s.id_part ".
+					"AND id_company = ".$id_company." ".
+					"AND p.id_currency = c.id_currency;";
 			$stmt = $con->query($sql);
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 		} catch(PDOException $e) {
@@ -40,6 +87,28 @@ class Reporting {
 					"AND s.id_company = ".$id_company." ".
 					"AND v.id_vehicle = m.id_vehicle ".
 					"AND (m.date_endmaintenance > NOW() OR m.date_endmaintenance = '0000-00-00');";
+			$stmt = $con->query($sql);
+			return $stmt->fetchAll(PDO::FETCH_OBJ);
+		} catch(PDOException $e) {
+			return array("error" => $e->getMessage());
+		}
+    }
+	
+    /**
+     * Description.
+     *
+     * @url GET /companies/$id_company/reporting/reference/$reference/localisation
+     */
+    public function getPartsBySite($id_company = null, $reference = null) {
+		try {
+			global $con;
+			$sql = 	"SELECT name, IFNULL(SUM(quanty), 0) AS partssum ".
+					"FROM parts p, stock s, sites si ".
+					"WHERE p.reference = '".$reference."' ".
+					"AND p.id_part = s.id_part ".
+					"AND s.id_site = si.id_site ".
+					"AND si.id_company = ".$id_company." ".
+					"GROUP BY name;";
 			$stmt = $con->query($sql);
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 		} catch(PDOException $e) {
@@ -78,6 +147,24 @@ class Reporting {
 					"FROM docsadministrative ".
 					"WHERE id_company = ".$id_company." ".
 					"AND NOW() + INTERVAL 15 DAY >= date_nextcontrol";
+			$stmt = $con->query($sql);
+			return $stmt->fetchAll(PDO::FETCH_OBJ);
+		} catch(PDOException $e) {
+			return array("error" => $e->getMessage());
+		}
+    }
+	
+	/**
+     * Description.
+     *
+     * @url GET /companies/$id_company/reporting/parts/all
+     */
+    public function getAllParts($id_company = null) {
+		try {
+			global $con;
+			$sql = 	"SELECT * ".
+					"FROM parts p ".
+					"WHERE id_company = ".$id_company.";";
 			$stmt = $con->query($sql);
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 		} catch(PDOException $e) {
