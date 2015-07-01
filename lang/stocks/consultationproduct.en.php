2242bc5053df49639899f44c57f37301de94011f
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:21:40 +0200

    Consultation Product changes & Refactoring

---
+++ lang/stocks/consultationproduct.en.php
@@ -0,0 +1,36 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+ 
+$stocks['TITLE'] = 'Stock consultation';
+$stocks['SELECT'] = ' Please select a site';
+$stocks['PRODUCT'] = 'Product';
+$stocks['SITELIST'] = 'Sites list';
+$stocks['ALLSITES'] = 'All sites';
+$stocks['BACK'] = ' Back';
+
+// Jquery table
+
+$stocks['REFERENCE'] = ' Reference';
+$stocks['DES'] = ' Designation';
+$stocks['BRAND'] = ' Brand';
+$stocks['COM'] = ' Comment';
+$stocks['PRICE'] = ' Buying price';
+$stocks['COMP'] = ' Company';
+$stocks['FAM'] = ' Family';
+$stocks['QUANTY'] = ' Quantity';
+$stocks['BAY'] = ' Bay';
+$stocks['DRIVE'] = ' Driveway';
+$stocks['POS'] = ' Position';
+$stocks['RACK'] = ' Rack';
+$stocks['SITE'] = ' Site';
+$stocks['TYPE'] = ' Kind stock';
+$stocks['LOCKER'] = ' Locker';
+$stocks['STOREHOUSE'] = ' Store House';
+
+?>
