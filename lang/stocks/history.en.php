e1bb1510e8f1157044c625fcfd2d3af2654fdb24
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:21:55 +0200

    History changes & Refactoring

---
+++ lang/stocks/history.en.php
@@ -0,0 +1,28 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['OUTPUTS'] = 'Outputs list';
+$stocks['ALLOUTPUTS'] = 'All outputs';
+$stocks['WORDING'] = 'Wording';
+$stocks['PRODUCT'] = 'Product';
+$stocks['RECIPIENT'] = 'Recipient';
+$stocks['QUANTITY'] = 'Quantity';
+$stocks['TRANSFERTDATE'] = 'Transfer date';
+$stocks['VALIDATIONDATE'] = 'Validation date';
+$stocks['TITLE'] = "Historical Review of outputs";
+$stocks['SELECT'] = 'Please select an output';
+$stocks['HISTORY'] = 'History';
+$stocks['SUBMIT'] = 'Save';
+$stocks['RESET'] = 'Reset';
+$stocks['BACK'] = ' Back';
+
+
+
+?>
---
+++ lang/stocks/history.fr.php
@@ -0,0 +1,28 @@
+<?php
+/*
+------------------
+Language: Français
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['OUTPUTS'] = 'Liste des sorties';
+$stocks['ALLOUTPUTS'] = 'Toutes les sorties';
+$stocks['WORDING'] = 'Libellé';
+$stocks['PRODUCT'] = 'Produit';
+$stocks['RECIPIENT'] = 'Déstinataire';
+$stocks['QUANTITY'] = 'Quantité';
+$stocks['TRANSFERTDATE'] = 'Date de transfert';
+$stocks['VALIDATIONDATE'] = 'Date de validation';
+$stocks['TITLE'] = "Consultation de l'historique des sorties";
+$stocks['SELECT'] = 'Veuillez sélectionner une sortie';
+$stocks['HISTORY'] = 'Historique';
+$stocks['SUBMIT'] = 'Valider';
+$stocks['RESET'] = 'Réinitialiser';
+$stocks['BACK'] = ' Retour';
+
+
+
+?>
