cbad5f9a0560223de6b7106208744df950e403b4
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:26:37 +0200

    Edit table of stock

---
+++ lang/stocks/edit.en.php
@@ -0,0 +1,33 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+ 
+$stocks['TITLE'] = 'Stock editor (update)';
+$stocks['SELECT'] = ' Please select a site';
+$stocks['PRODUCT'] = 'Product';
+$stocks['SITELIST'] = 'Sites list';
+$stocks['ALLSITES'] = 'All sites';
+$stocks['BACK'] = ' Back';
+
+// Jquery table
+
+$stocks['REFERENCE'] = ' Reference';
+$stocks['DES'] = ' Designation';
+$stocks['PRICE'] = ' Buying price';
+$stocks['COMP'] = ' Company';
+$stocks['FAM'] = ' Family';
+$stocks['QUANTY'] = ' Quantity';
+$stocks['BAY'] = ' Bay';
+$stocks['DRIVE'] = ' Driveway';
+$stocks['POS'] = ' Position';
+$stocks['RACK'] = ' Rack';
+$stocks['SITE'] = ' Site';
+$stocks['TYPE'] = ' Kind stock';
+$stocks['LOCKER'] = ' Locker';
+
+?>
