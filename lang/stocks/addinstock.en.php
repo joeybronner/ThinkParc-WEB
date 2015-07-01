92612c439b417c9cf5dcb355eb4c78797514757a
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:23:04 +0200

    Add in Stock changes & Refactoring

---
+++ lang/stocks/addinstock.en.php
@@ -0,0 +1,36 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+ 
+$stocks['TITLE'] = 'Add product in stock';
+$stocks['REFERENCE'] = 'Reference';
+$stocks['EXIST'] = 'Exist';
+$stocks['NOTEXIST'] = 'Not exist';
+$stocks['QUANTITY'] = 'Quantity';
+$stocks['SITE'] = 'Site';
+$stocks['KINDS'] = 'Kinds';
+$stocks['MEASURE'] = 'Measure';
+$stocks['QUANTITYANDMEASURE'] = 'Quantity and Measure';
+$stocks['QTE'] = 'quantity';
+$stocks['MAGASIN'] = 'Magasin No';
+$stocks['DRIVE'] = 'Driveway';
+$stocks['BAY'] = 'Bay';
+$stocks['RACK'] = 'Rack';
+$stocks['LOCKER'] = 'Locker';
+$stocks['POSITION'] = 'Position';
+$stocks['LOCATIONANDTYPE'] = 'Equivalency & * Stock kind';
+$stocks['OPTIONAL'] = 'optional';
+$stocks['STOREHOUSE'] = 'Store House';
+$stocks['ASSIGNMENTANDSTOREHOUSE'] = 'Assignment & Store house';
+$stocks['LOCATION'] = 'Location';
+$stocks['SUBMIT'] = 'Save';
+$stocks['RESET'] = 'Reset';
+$stocks['BACK'] = ' Back';
+
+?>
+?>
---
+++ lang/stocks/addinstock.fr.php
@@ -0,0 +1,38 @@
+<?php
+/*
+------------------
+Language: Français
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['TITLE'] = 'Ajout produit en stock';
+$stocks['REFERENCE'] = 'Référence';
+$stocks['EXIST'] = 'Existant';
+$stocks['NOTEXIST'] = 'Inéxistant';
+$stocks['QUANTITY'] = 'Quantité';
+$stocks['SITE'] = 'Site';
+$stocks['KINDS'] = 'Type';
+$stocks['MEASURE'] = 'Mesure';
+$stocks['QUANTITYANDMEASURE'] = 'Quantité et Mesure';
+$stocks['QTE'] = 'quantité';
+$stocks['MAGASIN'] = 'Magasin No';
+$stocks['DRIVE'] = 'Allée';
+$stocks['BAY'] = 'Travée';
+$stocks['RACK'] = 'Etage';
+$stocks['LOCKER'] = 'Casier';
+$stocks['POSITION'] = 'Position';
+$stocks['LOCATIONANDTYPE'] = 'Equivalence & * Type stock';
+$stocks['OPTIONAL'] = 'facultatif';
+$stocks['STOREHOUSE'] = 'Magasin';
+$stocks['ASSIGNMENTANDSTOREHOUSE'] = 'Affectation et magasin';
+$stocks['LOCATION'] = 'Emplacements';
+$stocks['SUBMIT'] = 'Valider';
+$stocks['RESET'] = 'Réinitialiser';
+$stocks['BACK'] = ' Retour';
+
+
+
+?>
