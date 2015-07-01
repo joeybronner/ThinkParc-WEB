f466c576e516f62c69413c4cb71722bb4a34596d
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:21:19 +0200

    Add product changes & Refactoring

---
+++ lang/stocks/addproduct.en.php
@@ -0,0 +1,28 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['TITLE'] = 'Add a product';
+$stocks['BRAND'] = 'Brand';
+$stocks['COM'] = 'Commentaire';
+$stocks['BRANDANDCOM'] = 'Brand & Commentaire';
+$stocks['FAMILY'] = 'Family';
+$stocks['UNDERFAMILY'] = 'Under family';
+$stocks['UNDERFAMILY2'] = 'Under family 2';
+$stocks['REFANDDESIGNATION'] = 'manufacturer reference & Designation';
+$stocks['REFERENCE'] = 'Reference';
+$stocks['DESIGNATION'] = 'Designation';
+$stocks['PRICE'] = 'Buying price';
+$stocks['SUBMIT'] = 'Save';
+$stocks['RESET'] = 'Reset';
+$stocks['BACK'] = ' Back';
+
+
+
+?>
---
+++ lang/stocks/addproduct.fr.php
@@ -0,0 +1,28 @@
+<?php
+/*
+------------------
+Language: Français
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['TITLE'] = 'Ajouter un produit';
+$stocks['BRAND'] = 'Marque';
+$stocks['COM'] = 'Commentaire';
+$stocks['BRANDANDCOM'] = 'Marque & Commentaire';
+$stocks['FAMILY'] = 'Famille';
+$stocks['UNDERFAMILY'] = 'Sous famille';
+$stocks['UNDERFAMILY2'] = 'Sous famille 2';
+$stocks['REFANDDESIGNATION'] = 'Référence constructeur & Désignation';
+$stocks['REFERENCE'] = 'Reference';
+$stocks['DESIGNATION'] = 'Designation';
+$stocks['PRICE'] = "Prix d'achat";
+$stocks['SUBMIT'] = 'Valider';
+$stocks['RESET'] = 'Réinitialiser';
+$stocks['BACK'] = ' Retour';
+
+
+
+?>
