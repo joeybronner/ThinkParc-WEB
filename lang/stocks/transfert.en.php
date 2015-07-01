520d41c37f185baf8f480ea0529de27a5f7deb69
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:22:14 +0200

    Transfert changes & Refactoring

---
+++ lang/stocks/transfert.en.php
@@ -0,0 +1,38 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['LIST'] = 'Sites list';
+$stocks['TITLE'] = "Product selection to transfer";
+$stocks['SELECT1'] = 'Select the site sender';
+$stocks['SELECT2'] = 'Select the destination site';
+$stocks['TRANSFER'] = 'Transfer';
+$stocks['INPUT'] = 'Enter a label';
+$stocks['INPUTNAME'] = 'Transfer Label';
+$stocks['SUBMIT'] = 'Save';
+$stocks['RESET'] = 'Reset';
+$stocks['BACK'] = ' Back';
+$stocks['BREAK'] = ' Out of stock';
+
+
+// Jquery table
+
+$stocks['REFERENCE'] = ' Reference';
+$stocks['QUANTY'] = ' Quantity';
+$stocks['BAY'] = ' Bay';
+$stocks['DRIVE'] = ' Driveway';
+$stocks['POSITION'] = ' Position';
+$stocks['RACK'] = ' Rack';
+$stocks['SITE'] = ' Site';
+$stocks['LOCKER'] = ' Locker';
+$stocks['AMOUNT'] = ' Transfer amount';
+
+
+
+?>
---
+++ lang/stocks/transfert.fr.php
@@ -0,0 +1,37 @@
+<?php
+/*
+------------------
+Language: Français
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['LIST'] = 'Liste des sites';
+$stocks['TITLE'] = "Choix du produit à transferer";
+$stocks['SELECT1'] = 'Sélectionner le site destinateur';
+$stocks['SELECT2'] = 'Sélectionner le site destinataire';
+$stocks['TRANSFER'] = 'Transfert';
+$stocks['INPUT'] = 'Saisir un libellé';
+$stocks['INPUTNAME'] = 'Libellé du transfert';
+$stocks['SUBMIT'] = 'Valider';
+$stocks['RESET'] = 'Réinitialiser';
+$stocks['BACK'] = ' Retour';
+$stocks['BREAK'] = ' Rupture de stock';
+
+// Jquery table
+
+$stocks['REFERENCE'] = ' Référence';
+$stocks['QUANTY'] = ' Quantité';
+$stocks['BAY'] = ' Travée';
+$stocks['DRIVE'] = ' Allée';
+$stocks['POSITION'] = ' Position';
+$stocks['RACK'] = ' Etage';
+$stocks['SITE'] = ' Site';
+$stocks['LOCKER'] = ' Casier';
+$stocks['AMOUNT'] = ' Nombre à transferer';
+
+
+
+?>
