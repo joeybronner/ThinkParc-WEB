d54bb28c12ac2e6847eff8f0f57a5b031ee9b567
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-01 19:22:07 +0200

    Reception changes & Refactoring

---
+++ lang/stocks/reception.en.php
@@ -0,0 +1,38 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['LIST'] = 'Receptions expectations';
+$stocks['TITLE'] = 'Choice of bistros';
+$stocks['SELECTTRANSFER'] = 'Select transfer';
+$stocks['TABLETITLE'] = 'Reception expectations';
+$stocks['RECEIVE'] = 'Receive';
+$stocks['SUBMIT'] = 'Submit';
+$stocks['RESET'] = 'Reset';
+$stocks['BACK'] = ' Back';
+$stocks['BREAK'] = ' Out of Stock';
+
+// Jquery table
+
+$stocks['REFERENCE'] = ' Reference';
+$stocks['QUANTY'] = ' Quantity';
+$stocks['STOREHOUSE'] = ' Store House';
+$stocks['BAY'] = ' Bay';
+$stocks['DATE'] = ' Date';
+$stocks['DRIVE'] = ' Driveway';
+$stocks['POSITION'] = ' Position';
+$stocks['RACK'] = ' Rack';
+$stocks['RECIPIENT'] = ' Recipient';
+$stocks['LABEL'] = ' Label';
+$stocks['LOCKER'] = ' Locker';
+$stocks['AMOUNT'] = ' Transfer Amount';
+
+
+
+?>
---
+++ lang/stocks/reception.fr.php
@@ -0,0 +1,38 @@
+<?php
+/*
+------------------
+Language: Français
+------------------
+*/
+ 
+$stocks = array();
+
+
+$stocks['LIST'] = 'Receptions en attentes';
+$stocks['TITLE'] = 'Choix de la récéption';
+$stocks['SELECTTRANSFER'] = 'Sélectionner le transfert';
+$stocks['TABLETITLE'] = 'Reception en attentes';
+$stocks['RECEIVE'] = 'Récéptionner';
+$stocks['SUBMIT'] = 'Valider';
+$stocks['RESET'] = 'Réinitialiser';
+$stocks['BACK'] = ' Retour';
+$stocks['BREAK'] = ' Rupture de stock';
+
+// Jquery table
+
+$stocks['REFERENCE'] = ' Référence';
+$stocks['STOREHOUSE'] = ' Magasin';
+$stocks['QUANTY'] = ' Quantité';
+$stocks['BAY'] = ' Travée';
+$stocks['DATE'] = ' Date';
+$stocks['DRIVE'] = ' Allée';
+$stocks['POSITION'] = ' Position';
+$stocks['RACK'] = ' Etagère';
+$stocks['RECIPIENT'] = ' Destinataire';
+$stocks['LABEL'] = ' Libellé';
+$stocks['LOCKER'] = ' Casier';
+$stocks['AMOUNT'] = ' Nombre à transferer';
+
+
+
+?>
