45eae1e9518973a8c3a91e284750432f6b940aeb
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-03 21:38:39 +0200

    Addsite done

--- src/options/addsite.js
+++ src/options/addsite.js
@@ -5,66 +5,68 @@
  * @author Said KHALID
  * @version 1.0
  */
- 
-
-
 /** Method called on page loading */
+$(function onLoad() {
+   getcompany();
+});
 
-	$(function onLoad() 
-	{
-		getcompany();			
-	});	
-		
-/** Allows to add a new site for one company */		
-	function addsite(id_company, name, adress1, adress2, adress3, city, country) {
-         									
-	 var id_company = document.getElementById("id_company").value;
-	 var name = document.getElementById("name").value;
-	 var adress1 = document.getElementById("adress1").value;
-  	 var adress2 = document.getElementById("adress2").value;
-	 var adress3 = document.getElementById("adress3").value;
-	 var city = document.getElementById("city").value;
-	 var country = document.getElementById("country").value;
-					
-      $.ajax({
-         									
-       	type: 		"POST",
-       	url:		"http://www.think-parc.com/webservice/v1/companies/options/addsite/name/"+name+"/id_company/"+id_company+"/adress1/"+adress1+"/adress2/"+adress2+"/adress3/"+adress3+"/city/"+city+"/country/"+country,  
-       	success:	function(data) {
-       	$(document).ready(function() {
-    		$.toast({heading: "Success",text: "Site successfully added.", icon: "success"});
-		});
-         														
-				},
-         		error:		function(xhr, status, error) {
-         		$(document).ready(function() {
-         			$.toast({heading: "Error",text: "Error", icon: "error"});
-         				});
-         					}
-         	});
-         											
-       	};
-			
-			
-         	
+/** Allows to add a new site for one company */
+function addsite(id_company, name, adress1, adress2, adress3, city, country) {
+
+   var id_company = document.getElementById("id_company").value;
+   var name = document.getElementById("name").value;
+   var adress1 = document.getElementById("adress1").value;
+   var adress2 = document.getElementById("adress2").value;
+   var adress3 = document.getElementById("adress3").value;
+   var city = document.getElementById("city").value;
+   var country = document.getElementById("country").value;
+
+   $.ajax({
+
+      type: "POST",
+      url: "http://www.think-parc.com/webservice/v1/companies/options/addsite/name/" + name + "/id_company/" + id_company + "/adress1/" + adress1 + "/adress2/" + adress2 + "/adress3/" + adress3 + "/city/" + city + "/country/" + country,
+      success: function(data) {
+         document.getElementById("addsite").reset();
+         $(document).ready(function() {
+            $.toast({
+               heading: "Success",
+               text: "Site successfully added.",
+               icon: "success"
+            });
+         });
+
+      },
+      error: function(xhr, status, error) {
+         $(document).ready(function() {
+            $.toast({
+               heading: "Error",
+               text: "Error",
+               icon: "error"
+            });
+         });
+      }
+   });
+
+};
+
+
+
 /** Allows to get all companies on a list */
-	 function getcompany()
-	 {
-					
-       $.ajax({
-       method: 	"GET",
-       url:		"http://think-parc.com/webservice/v1/companies/options/getcompany",  
-       success:	function(data) {
-        var response = JSON.parse(data);
-		var content = '<option selected disabled><?php echo $options['COMPANY'];?></option>';
-         for (var i = 0; i<response.length; i++) 
-         	{
-         		 content = content + '<option value="'+response[i].id_company+'">'+ response[i].name +'</option>';
-         	}
-         document.getElementById("id_company").innerHTML = content;
-							
+function getcompany() {
+
+   $.ajax({
+      method: "GET",
+      url: "http://think-parc.com/webservice/v1/companies/options/getcompany",
+      success: function(data) {
+         var response = JSON.parse(data);
+         var content = '<option selected disabled>Company</option>';
+         for (var i = 0; i < response.length; i++) {
+            content = content + '<option value="' + response[i].id_company + '">' + response[i].name + '</option>';
          }
-			});
-	 };
+         document.getElementById("id_company").innerHTML = content;
+
+      }
+   });
+};
 
 /** End of file */
--- src/options/addsite.php
+++ src/options/addsite.php
@@ -20,11 +20,12 @@
  * =========================================================================*/
 ?>
 
-<html>
-  <head>
-	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<?php
+   <html>
+
+   <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <?php
 		if(!isset($_SESSION)) {
 			session_start();
 		}
@@ -44,103 +45,111 @@
 		else
 		include('../../lang/options/addsite.en.php');
 		/* 5. Import specific JavaScript file for this page */
-		echo '<script type="text/javascript" src="addmaintenance.js"></script>';
+		echo '<script type="text/javascript" src="addsite.js"></script>';
 	?>
-      
+
    </head>
+
    <body>
 
-	<!-- Include navbar with home, informations & logout shortcuts -->
-	<?php require(BASE_PATH . '/src/header/navbar.php'); ?>
-	
-	<!-- Hidden div(s) for JS values -->
-	<div id="fct_id_user" style="display: none;"><?php echo $_SESSION['fct_id_user']; ?></div>
+      <!-- Include navbar with home, informations & logout shortcuts -->
+      <?php require(BASE_PATH . '/src/header/navbar.php'); ?>
 
-	<!-- Background image for this page-->
-	<img src="../../images/zoom-bg-4.jpg" id="menu-img" class="main-img inactive" alt="FCT Partners">
-	
-	<!-- Page content -->
-	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
-		<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left margin-bottom-20">
-				<a href="../accueil.php?section=options">
-						<h5><i class="fa fa-chevron-left"></i><?php echo $options['BACK'];?></h5>
-				</a>
-			</div>
-		</div>
-	   <div class="templatemo-content">
-			<div class="black-bg btn-menu margin-bottom-20">
-				<h2><?php echo $options['ADDSITE'];?></h2>
-				<div class="panel-body">
-					<div class="row">
-						<div class="col-md-12 col-lg-12"> 
-							<form class="formimg" action="javascript:addsite(id_company, name, adress1, adress2, adress3, city, country);" method="post" enctype="multipart/form-data">
-								<table class="table-no-border">
-									<tbody>
-										<tr>
-											<td><h5>* <?php echo $options['COMPANY'];?></h5></td>
-										</tr>
-										<tr>
-											<td colspan="2">
-												 <select id="id_company" name="id_company" class="form-control">
-													<!-- Here are loaded company content -->
-												 </select>
-											</td>
-										</tr>
-										<tr>
-											<td><h5>* <?php echo $options['SITENAME'];?></h5></td>
-										</tr>
-										<tr>
-											<td colspan="2">
-												<input type="text" id="name" placeholder="<?php echo $options['NAME'];?>" class="form-control" required/>
-											</td>
-										</tr>
-										<tr>
-											<td><h5>* <?php echo $options['ADDRESS'];?></h5></td>
-										</tr>
-										<tr>
-											<td>
-												<input type="text" id="adress1" placeholder="<?php echo $options['ADRESS1'];?>" class="form-control" required/>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<input type="text" id="adress2" placeholder="<?php echo $options['ADRESS2'];?>" class="form-control" required/>
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<input type="text" id="adress3" placeholder="<?php echo $options['ADRESS3'];?>" class="form-control" required/>
-											</td>
-										</tr>
-										<tr>
-											<td><h5>* <?php echo $options['CITYANDCOUNTRY'];?></h5></td>
-										</tr>
-										<tr>
-											<td>
-												<input type="text" id="city" placeholder="<?php echo $options['CITY'];?>" class="form-control" required/>
-											</td>
-											<td>
-												<input type="text" id="country" placeholder="<?php echo $options['COUNTRY'];?>" class="form-control" required/>
-											</td>
-										</tr>
-										<tr>
-											<td colspan="3" align="right">
-												<input type="reset" value="<?php echo $options['RESET'];?>" class="btn btn-warning"/>
-												<input type="submit" class="btn btn-success" value="<?php echo $options['SUBMIT'];?>"/>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End page content -->
-	<!-- Include footer bar with language switch & global website informations -->
-	<?php include('../footer/footer.php'); ?>
-</body>
-</html>
+         <!-- Hidden div(s) for JS values -->
+         <div id="fct_id_company" style="display: none;">
+            <?php echo $_SESSION['fct_id_company']; ?>
+         </div>
+
+         <!-- Background image for this page-->
+         <img src="../../images/zoom-bg-4.jpg" id="menu-img" class="main-img inactive" alt="FCT Partners">
+
+         <!-- Page content -->
+         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
+            <div class="row">
+               <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left margin-bottom-20">
+                  <a href="../accueil.php?section=options">
+                     <h5><i class="fa fa-chevron-left"></i><?php echo $options['BACK'];?></h5>
+                  </a>
+               </div>
+            </div>
+            <div class="templatemo-content">
+               <div class="black-bg btn-menu margin-bottom-20">
+                  <h2><?php echo $options['ADDSITE'];?></h2>
+                  <div class="panel-body">
+                     <div class="row">
+                        <div class="col-md-12 col-lg-12">
+                           <form id="addsite" class="formimg" action="javascript:addsite(fct_id_company, name, adress1, adress2, adress3, city, country);" method="post" enctype="multipart/form-data">
+                              <table class="table-no-border">
+                                 <tbody>
+                                    <tr>
+                                       <td>
+                                          <h5>* <?php echo $options['COMPANY'];?></h5></td>
+                                    </tr>
+                                    <tr>
+                                       <td colspan="2">
+                                          <select id="id_company" name="id_company" class="form-control">
+                                             <!-- Here are loaded company content -->
+                                          </select>
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <h5>* <?php echo $options['SITENAME'];?></h5></td>
+                                    </tr>
+                                    <tr>
+                                       <td colspan="2">
+                                          <input type="text" id="name" placeholder="<?php echo $options['NAME'];?>" class="form-control" required/>
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <h5>* <?php echo $options['ADDRESS'];?></h5></td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <input type="text" id="adress1" placeholder="<?php echo $options['ADRESS1'];?>" class="form-control" required/>
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <input type="text" id="adress2" placeholder="<?php echo $options['ADRESS2'];?>" class="form-control" required/>
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <input type="text" id="adress3" placeholder="<?php echo $options['ADRESS3'];?>" class="form-control" required/>
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <h5>* <?php echo $options['CITYANDCOUNTRY'];?></h5></td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <input type="text" id="city" placeholder="<?php echo $options['CITY'];?>" class="form-control" required/>
+                                       </td>
+                                       <td>
+                                          <input type="text" id="country" placeholder="<?php echo $options['COUNTRY'];?>" class="form-control" required/>
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td colspan="3" align="right">
+                                          <input type="reset" value="<?php echo $options['RESET'];?>" class="btn btn-warning" />
+                                          <input type="submit" class="btn btn-success" value="<?php echo $options['SUBMIT'];?>" />
+                                       </td>
+                                    </tr>
+                                 </tbody>
+                              </table>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- End page content -->
+         <!-- Include footer bar with language switch & global website informations -->
+         <?php include('../footer/footer.php'); ?>
+   </body>
+
+   </html>
