a586dce26766e2bcf1fcc1536da7fc3c175e3097
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-03 21:38:21 +0200

    AddBrand

--- src/options/addbrand.js
+++ src/options/addbrand.js
@@ -5,32 +5,39 @@
  * @author Said KHALID
  * @version 1.0
  */
- 
- 
- /** Allows to add a new brand */
- 
- function addbrand(brand) {
-         									
-var brand = document.getElementById("brand").value;
-					
-         									
-$.ajax({
-         									
- type: 		"POST",
- url:		"http://www.think-parc.com/webservice/v1/companies/options/addbrand/"+brand,  
- success:	function(data) {
-         	$(document).ready(function() {
-         		$.toast({heading: "Success",text: "Brand successfully added.", icon: "success"});
-         		});
-         														
-					},
-         			error:		function(xhr, status, error) {
-         			$(document).ready(function() {
-         				$.toast({heading: "Error",text: "Error", icon: "error"});
-         					});
-         						}
-         		});
-         											
-      };
+/** Allows to add a new brand */
+function addbrand(brand) {
 
- /** End of file */
+   var brand = document.getElementById("brand").value;
+
+
+   $.ajax({
+
+      type: "POST",
+      url: "http://www.think-parc.com/webservice/v1/companies/options/addbrand/" + brand,
+      success: function(data) {
+         $(document).ready(function() {
+            document.getElementById("addbrand").reset();
+            $.toast({
+               heading: "Success",
+               text: "Brand successfully added.",
+               icon: "success"
+            });
+
+         });
+
+      },
+      error: function(xhr, status, error) {
+         $(document).ready(function() {
+            $.toast({
+               heading: "Error",
+               text: "Error",
+               icon: "error"
+            });
+         });
+      }
+   });
+
+};
+
+/** End of file */
--- src/options/addbrand.php
+++ src/options/addbrand.php
@@ -19,11 +19,12 @@
  * JJ/MM/AAAA | ...			   | ...			 							*
  * =========================================================================*/
 ?>
-<html>
+   <html>
+
    <head>
-	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<?php
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <?php
 		if(!isset($_SESSION)) {
 			session_start();
 		}
@@ -44,65 +45,73 @@
 		include('../../lang/options/addbrand.en.php');
 		
 		/* 5. Import specific JavaScript file for this page */
-		echo '<script type="text/javascript" src="addmaintenance.js"></script>';
+		echo '<script type="text/javascript" src="addbrand.js"></script>';
 	?>
-	<title><?php echo $options['TITLE_ADDBRAND'];?></title>
-</head>
-   <body>
-	
-	<!-- Include navbar with home, informations & logout shortcuts -->
-	<?php require(BASE_PATH . '/src/header/navbar.php'); ?>
-	
-	<!-- Hidden div(s) for JS values -->
-	<div id="fct_id_user" style="display: none;"><?php echo $_SESSION['fct_id_user']; ?></div>
+         <title>
+            <?php echo $options['TITLE_ADDBRAND'];?>
+         </title>
+   </head>
 
-	<!-- Background image for this page-->
-	<img src="../../images/zoom-bg-4.jpg" id="menu-img" class="main-img inactive" alt="FCT Partners">
-	
-	<!-- Page content -->
-	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
-		<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left margin-bottom-20">
-				<a href="../accueil.php?section=options">
-						<h5><i class="fa fa-chevron-left"></i><?php echo $options['BACK'];?></h5>
-				</a>
-			</div>
-		</div>
-	   <div class="templatemo-content">
-			<div class="black-bg btn-menu margin-bottom-20">
-				<h2><?php echo $options['TITLE_ADDBRAND'];?></h2>
-				<div class="panel-body">
-					<div class="row">
-						<div class="col-md-12 col-lg-12"> 
-							<form class="formimg" action="javascript:addbrand(brand);" method="post" enctype="multipart/form-data">
-								<table class="table-no-border">
-									<tbody>
-										<tr>
-											<td><h5>* <?php echo $options['BRAND'];?></h5></td>
-										</tr>
-										<tr>
-											<td>
-												<input type="text" id="brand" placeholder="<?php echo $options['BRAND'];?>" class="form-control" required/>
-											</td>
-										</tr>
-										
-										<tr>
-											<td colspan="3" align="right">
-												<input type="reset" value="<?php echo $options['RESET'];?>" class="btn btn-warning"/>
-												<input type="submit" class="btn btn-success" value="<?php echo $options['SUBMIT'];?>"/>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- End page content -->
-	<!-- Include footer bar with language switch & global website informations -->
-	<?php include('../footer/footer.php'); ?>
-</body>
-</html>
+   <body>
+
+      <!-- Include navbar with home, informations & logout shortcuts -->
+      <?php require(BASE_PATH . '/src/header/navbar.php'); ?>
+
+         <!-- Hidden div(s) for JS values -->
+         <div id="fct_id_company" style="display: none;">
+            <?php echo $_SESSION['fct_id_company']; ?>
+         </div>
+
+         <!-- Background image for this page-->
+         <img src="../../images/zoom-bg-4.jpg" id="menu-img" class="main-img inactive" alt="FCT Partners">
+
+         <!-- Page content -->
+         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
+            <div class="row">
+               <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left margin-bottom-20">
+                  <a href="../accueil.php?section=options">
+                     <h5><i class="fa fa-chevron-left"></i><?php echo $options['BACK'];?></h5>
+                  </a>
+               </div>
+            </div>
+            <div class="templatemo-content">
+               <div class="black-bg btn-menu margin-bottom-20">
+                  <h2><?php echo $options['TITLE_ADDBRAND'];?></h2>
+                  <div class="panel-body">
+                     <div class="row">
+                        <div class="col-md-12 col-lg-12">
+                           <form id="addbrand" class="formimg" action="javascript:addbrand(brand);" method="post" enctype="multipart/form-data">
+                              <table class="table-no-border">
+                                 <tbody>
+                                    <tr>
+                                       <td>
+                                          <h5>* <?php echo $options['BRAND'];?></h5></td>
+                                    </tr>
+                                    <tr>
+                                       <td>
+                                          <input type="text" id="brand" placeholder="<?php echo $options['BRAND'];?>" class="form-control" required/>
+                                       </td>
+                                    </tr>
+
+                                    <tr>
+                                       <td colspan="3" align="right">
+                                          <input type="reset" value="<?php echo $options['RESET'];?>" class="btn btn-warning" />
+                                          <input type="submit" class="btn btn-success" value="<?php echo $options['SUBMIT'];?>" />
+                                       </td>
+                                    </tr>
+                                 </tbody>
+                              </table>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+         <!-- End page content -->
+         <!-- Include footer bar with language switch & global website informations -->
+         <?php include('../footer/footer.php'); ?>
+   </body>
+
+   </html>
