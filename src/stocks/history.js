44966aac4bfb1c674912b8906b1deb4e24b72218
Khalidsaid <Khalidsaid.box@gmail.com>
2015-07-03 21:39:15 +0200

    History done

--- src/stocks/history.js
+++ src/stocks/history.js
@@ -5,89 +5,97 @@
  * @author Said KHALID
  * @version 1.0
  */
-
 $(function onLoad() {
-				var id_company = <?php echo $_SESSION['fct_id_company']; ?>;
-				document.getElementById("productblock").style.display = "none";
-				getreleaseproduct(id_company);
-			});
-			
-			
-			function getreleaseproduct(id_company){
-		 
-				var id_company = <?php echo $_SESSION['fct_id_company']; ?>;
-		 
-            	$.ajax({
-         		method: 	"GET",
-         		url:		"http://think-parc.com/webservice/v1/companies/"+id_company+"/release", 
-         		success:	function(data) {
-         						
-								var response = JSON.parse(data);
-								var content = '<option selected disabled><?php echo $stocks['OUTPUTS'];?></option>';
-								content = content + '<option value="all"><?php echo $stocks['ALLOUTPUTS'];?></option>';
-								
-         						for (var i = 0; i<response.length; i++) 
-         						{
-									content = content + '<option value="'+response[i].title+'"> date : '+ response[i].transferdate +' --- <?php echo $stocks['WORDING'];?> : '+ response[i].title +' </option>';
-         						}
-         						
-								document.getElementById("list").innerHTML = content;
-         					}
-         	});
-         };
-			
-			
-			function gethistory(title, id_company) {
-			
-			var id_company = <?php echo $_SESSION['fct_id_company']; ?>;
-			
-			$.ajax({
-			method: 	"GET",
-			url:		"http://think-parc.com/webservice/v1/companies/stocks/historylist/"+title+"/company/"+id_company, 
-			success:	function(data) {
-							var response = JSON.parse(data);
-							var dataSet = new Array(response.length);
-							
-							for (var i = 0; i<response.length; i++) 
-							{
-							
-									
-								dataSet[i] = new Array( response[i].title,  
-														response[i].reference,
-														response[i].companyname,
-														response[i].quantity,
-														response[i].transferdate,
-														response[i].validationdate);
-							}
-						
-							document.getElementById("productblock").style.display = "block";
-							
-							$('#stock').html( '<table cellspacing="0" width="100%" id="example"></table>' );
-							
-							$('#example').dataTable( {
-								"data": dataSet,
-								   "scrollX": true,
-								   "bPaginate": true,
-								   "bLengthChange": true,
-								   "bStateSave": true,
-								   "bFilter": true,
-								   "bSort": true,
-								   "bInfo": true,
-								   "bAutoWidth": true,
-								"columns": [
-									{ "title": "<?php echo $stocks['WORDING'];?>" , "class": "center fctbw"},
-									{ "title": "<?php echo $stocks['PRODUCT'];?>" , "class": "center fctbw" },
-									{ "title": "<?php echo $stocks['RECIPIENT'];?>", "class": "center fctbw" },
-									{ "title": "<?php echo $stocks['QUANTITY'];?>", "class": "center fctbw" },
-									{ "title": "<?php echo $stocks['TRANSFERTDATE'];?>", "class": "center fctbw" },
-									{ "title": "<?php echo $stocks['VALIDATIONDATE'];?>", "class": "center fctbw" }
-								]
-							} );   
-						},
-			error:		function(data) {
-							
-						}
-		});
-	};	 
-	
- /** End of file */
+   var id_company = document.getElementById('fct_id_company').innerHTML;
+   document.getElementById("productblock").style.display = "none";
+   getreleaseproduct(id_company);
+});
+
+
+function getreleaseproduct(id_company) {
+
+   var id_company = document.getElementById('fct_id_company').innerHTML;
+
+   $.ajax({
+      method: "GET",
+      url: "http://think-parc.com/webservice/v1/companies/" + id_company + "/release",
+      success: function(data) {
+
+         var response = JSON.parse(data);
+         var content = '<option selected disabled>Sortie</option>';
+         content = content + '<option value="all">Toutes les sorties</option>';
+
+         for (var i = 0; i < response.length; i++) {
+            content = content + '<option value="' + response[i].title + '"> date : ' + response[i].transferdate + ' --- libelle : ' + response[i].title + ' </option>';
+         }
+
+         document.getElementById("list").innerHTML = content;
+      }
+   });
+};
+
+
+function gethistory(title, id_company) {
+
+   var id_company = document.getElementById('fct_id_company').innerHTML;
+
+   $.ajax({
+      method: "GET",
+      url: "http://think-parc.com/webservice/v1/companies/stocks/historylist/" + title + "/company/" + id_company,
+      success: function(data) {
+         var response = JSON.parse(data);
+         var dataSet = new Array(response.length);
+
+         for (var i = 0; i < response.length; i++) {
+
+
+            dataSet[i] = new Array(response[i].title,
+               response[i].reference,
+               response[i].companyname,
+               response[i].quantity,
+               response[i].transferdate,
+               response[i].validationdate);
+         }
+
+         document.getElementById("productblock").style.display = "block";
+
+         $('#stock').html('<table cellspacing="0" width="100%" id="example"></table>');
+
+         $('#example').dataTable({
+            "data": dataSet,
+            "scrollX": true,
+            "bPaginate": true,
+            "bLengthChange": true,
+            "bStateSave": true,
+            "bFilter": true,
+            "bSort": true,
+            "bInfo": true,
+            "bAutoWidth": true,
+            "columns": [{
+               "title": "Wording",
+               "class": "center fctbw"
+            }, {
+               "title": "Product",
+               "class": "center fctbw"
+            }, {
+               "title": "Recipient",
+               "class": "center fctbw"
+            }, {
+               "title": "Quantity",
+               "class": "center fctbw"
+            }, {
+               "title": "Transfer date",
+               "class": "center fctbw"
+            }, {
+               "title": "Validation date",
+               "class": "center fctbw"
+            }]
+         });
+      },
+      error: function(data) {
+
+      }
+   });
+};
+
+/** End of file */
--- src/stocks/history.php
+++ src/stocks/history.php
@@ -19,11 +19,12 @@
  * JJ/MM/AAAA | ...			   | ...			 							*
  * =========================================================================*/
 ?>
-<html>
+   <html>
+
    <head>
-	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<?php
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <?php
 		if(!isset($_SESSION)) {
 			session_start();
 		}
@@ -39,80 +40,82 @@
 		
 		/* 4. Import language values: French or English files */
 		if($_SESSION['fct_lang'] == 'FR')
-		include('../../lang/options/history.fr.php');
+		include('../../lang/stocks/history.fr.php');
 		else
-		include('../../lang/options/history.en.php');
+		include('../../lang/stocks/history.en.php');
 		
 		/* 5. Import specific JavaScript file for this page */
 		echo '<script type="text/javascript" src="history.js"></script>';
 		?>
-		</head>
-		<body>
-		<?php include('../header/navbar.php'); ?>
-		
-		<img src="../../images/zoom-bg-4.jpg" id="menu-img" class="main-img inactive" alt="FCT Partners"/>
-		
-	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
-		<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left margin-bottom-20">
-				<a href="../accueil.php?section=products">
-					<h5><i class="fa fa-chevron-left"></i><?php echo $stocks['BACK'];?></h5>
-				</a>
-			</div>
-		</div>
-		   <div class="templatemo-content">
-			    <div class="black-bg btn-menu margin-bottom-20">
-					<h2><?php echo $stocks['TITLE'];?></h2>
-						<div class="panel-body">
-							<div class="row">
-								<div class="col-md-12 col-lg-12"> 
-									<form>
-										<table class="table-no-border">
-												<tr>
-													<td><h5><?php echo $stocks['SELECT'];?></h5></td>
-												</tr>
-												<tr>
-													<td>
-													<script>
-														var id_company = <?php echo $_SESSION['fct_id_company']; ?>;
-													</script>
-														<select id="list" name="list" class="form-control" onchange="gethistory(this.value);">
-															<!-- Retrieve all products with an AJAX [GET] query -->
-														</select>
-													</td>
-												</tr>
-										</table>
-									</form>
-								</div>
-							</div>
-						</div>
-				</div>	
-			</div>
-	</div>
-		
-	<div id="productblock">	
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xs-offset-0 col-sm-offset-0 toppad">
-		   <div class="templatemo-content">
-				<div class="black-bg btn-menu margin-bottom-20">
-					<h2><?php echo $stocks['HISTORY'];?></h2>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-md-12 col-lg-12"> 
-								<form>
-									<div class="col-md-12 col-lg-12">
-										<div id="stock">
-									    </div>	
-									</div>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>				
-	</div>
-	<!-- End page content -->
-	<!-- Include footer bar with language switch & global website informations -->
-	<?php include('../footer/footer.php'); ?>
-  </body>
-</html>
+   </head>
+
+   <body>
+      <?php include('../header/navbar.php'); ?>
+
+         <img src="../../images/zoom-bg-4.jpg" id="menu-img" class="main-img inactive" alt="FCT Partners" />
+
+         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
+            <div class="row">
+               <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left margin-bottom-20">
+                  <a href="../accueil.php?section=products">
+                     <h5><i class="fa fa-chevron-left"></i><?php echo $stocks['BACK'];?></h5>
+                  </a>
+               </div>
+            </div>
+            <div class="templatemo-content">
+               <div class="black-bg btn-menu margin-bottom-20">
+                  <h2><?php echo $stocks['TITLE'];?></h2>
+                  <div class="panel-body">
+                     <div class="row">
+                        <div class="col-md-12 col-lg-12">
+                           <form>
+                              <table class="table-no-border">
+                                 <tr>
+                                    <td>
+                                       <h5><?php echo $stocks['SELECT'];?></h5></td>
+                                 </tr>
+                                 <tr>
+                                    <td>
+                                       <!-- Hidden input for JS values -->
+                                       <input type="hidden" id="fct_id_company" name="fct_id_company" value="<?php echo $_SESSION['fct_id_company'];?>" />
+                                       <select id="list" name="list" class="form-control" onchange="gethistory(this.value);">
+                                          <!-- Retrieve all products with an AJAX [GET] query -->
+                                       </select>
+                                    </td>
+                                 </tr>
+                              </table>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <div id="productblock">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xs-offset-0 col-sm-offset-0 toppad">
+               <div class="templatemo-content">
+                  <div class="black-bg btn-menu margin-bottom-20">
+                     <h2><?php echo $stocks['HISTORY'];?></h2>
+                     <div class="panel-body">
+                        <div class="row">
+                           <div class="col-md-12 col-lg-12">
+                              <form>
+                                 <div class="col-md-12 col-lg-12">
+                                    <div id="stock">
+                                    </div>
+                                 </div>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- End page content -->
+         <!-- Include footer bar with language switch & global website informations -->
+         <?php include('../footer/footer.php'); ?>
+   </body>
+
+   </html>
